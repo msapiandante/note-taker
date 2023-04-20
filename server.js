b75599f2032e3291b103ee5731dd22b287ca0935
Melanie S <melanie.sapiandante@gmail.com>
2023-04-19 21:10:56 -0400

    created HTML routes to return notes.html file and index.html file. Added Get and Post api routes to add to db.json file

---
+++ server.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, 'index.html'))
+);
+
+// GET request for notes
+app.get('/notes', (req, res) => {
+
+  res.status(200).json(`${req.method} request received to get notes`);
+  res.sendFile(path.join(__dirname, 'notes.html'));
+
+  // Log our request to the terminal
+  console.info(`${req.method} request received to get notes`);
+});
+
+// POST request to add a note
+app.post('/notes', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a note`);
+
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNotes = {
+      title,
+      text,
+    };
+
+    // Obtain existing notes
+    fs.readFile('./db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // Convert string into JSON object
+        const parsedNotes = JSON.parse(data);
+
+        // Add a new note
+        parsedNotes.push(newNotes);
+
+        // Write updated notes back to the file
+        fs.writeFile(
+          './db.json',
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated notes!')
+        );
+      }
+    });
+
+    const response = {
+      status: 'success',
+      body: newNotes,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in saving note.');
+  }
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
