6dd23db3dc39095fb9495b70b1b363a70d62c0d9
Melanie S <melanie.sapiandante@gmail.com>
2023-04-19 21:26:56 -0400

    linked uuid.js to server.js file by requiring uuid.js and then entered code calling unique id function within newNotes variable

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+// Helper method for generating unique ids
+const uuid = require('./helpers/uuid');
 
 const PORT = 3001;
 
@@ -39,6 +41,7 @@ app.post('/notes', (req, res) => {
     const newNotes = {
       title,
       text,
+      review_id: uuid(),
     };
 
     // Obtain existing notes
